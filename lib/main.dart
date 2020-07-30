842b3f4f7ce7ac18add1b0b74d41fcc350eda0e6
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2020-07-30 17:46:03 +0700

    home_screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scheduleapp/presentation/page/home_screen.dart';
 import 'package:scheduleapp/presentation/page/on_board.dart';
 
 
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: OnboardingMe(),
+      home: HomeScreen()
+//      OnboardingMe(),
       
     );
   }
