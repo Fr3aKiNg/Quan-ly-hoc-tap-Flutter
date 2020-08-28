bb69fc9705df3f6c46ff8a43ec4091a5d763815c
npdang1599 <57211054+npdang1599@users.noreply.github.com>
2020-08-28 13:31:48 +0700

    Thêm trang đọc báo

---
+++ lib/presentation/page/news_screen.dart
@@ -0,0 +1,398 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:scheduleapp/application/color_app.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+// Class ListItem: Cấu trúc của một bài báo
+class ListItem {
+  String title;
+  String subtitle;
+  String imgUrl;
+  String link;
+}
+
+
+class MyTabbedPage extends StatefulWidget {
+  const MyTabbedPage({Key key}) : super(key: key);
+
+  final String title = 'news feed';
+
+  @override
+  _MyTabbedPageState createState() => _MyTabbedPageState();
+}
+
+class _MyTabbedPageState extends State<MyTabbedPage>
+    with SingleTickerProviderStateMixin {
+
+  // rss url
+  static const List<String> FEED_URL = [
+    'https://thanhnien.vn/rss/giao-duc.rss',
+    'https://tuoitre.vn/rss/giao-duc.rss',
+   'https://nld.com.vn/giao-duc-khoa-hoc.rss',
+    //'https://vnexpress.net/rss/giao-duc.rss'
+
+  ];
+  RssFeed _feed;
+  List<RssFeed> _feedList = [];
+  //String _title;
+  //static const String loadingFeedMsg = 'Loading Feed...';
+  //static const String feedLoadErrorMsg = 'Error Loading Feed.';
+  //static const String feedOpenErrorMsg = 'Error Opening Feed.';
+  static const String placeholderImg = 'assets/no_image.png';
+  //GlobalKey<RefreshIndicatorState> _refreshKey;
+  List<ListItem> listItemDH = [];
+  List<ListItem> listItemCD = [];
+  List<ListItem> listItemC3 = [];
+  List<ListItem> listItemC2 = [];
+  List<ListItem> listItemC1 = [];
+  List<ListItem> listItemAll = [];
+
+  List<String> uniKeyword = ['đại học', 'đh'];
+  List<String> colKeyword = ['cao đẳng', 'cđ', 'trung cấp'];
+  List<String> higKeyword = [
+    'cấp 3',
+    'trung học phổ thông',
+    'thpt',
+    'khối 12',
+    'khối 11',
+    'khối 10',
+    'lớp 10',
+    'lớp 11',
+    'lớp 12'
+  ];
+  List<String> junHigKeyword = [
+    'cấp 2',
+    'trung học cơ sở',
+    'thcs',
+    'khối 9',
+    'khối 8',
+    'khối 7',
+    'lớp 6',
+    'lớp 9',
+    'lớp 8',
+    'lớp 7',
+    'lớp 6'
+  ];
+  List<String> eleKeyword = ['cấp 1', 'tiểu học', 'lớp 1'];
+  List<String> allTabKeyword = ['học sinh'];
+
+  /*updateTitle(title) {
+    setState(() {
+      _title = title;
+    });
+  }*/
+
+  updateFeed(feed) {
+    setState(() {
+      _feed = feed;
+    });
+    _feedList.add(_feed);
+  }
+
+  Future<void> openFeed(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: false,
+      );
+      return;
+    }
+    //updateTitle(feedOpenErrorMsg);
+  }
+
+  load() async {
+    //updateTitle(loadingFeedMsg);
+    for (int i = 0; i < FEED_URL.length; i++) {
+      print(FEED_URL[i]);
+      loadFeed(FEED_URL[i]).then((result) {
+        if (null == result || result.toString().isEmpty) {
+          print(
+              'empty-----------------------------------------------------------------');
+          // updateTitle(feedLoadErrorMsg);
+          return;
+        }
+        updateFeed(result);
+        // updateTitle(_feed.title);
+        getItems();
+      });
+    }
+  }
+
+  Future<RssFeed> loadFeed(url) async {
+    try {
+      final client = http.Client();
+      //final response = await client.get(FEED_URL);
+      final response = await client.get(url);
+      return RssFeed.parse(response.body);
+    } catch (e) {
+      //
+    }
+
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // _refreshKey = GlobalKey<RefreshIndicatorState>();
+    // updateTitle(widget.title);
+    load();
+  }
+
+  title(title) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  subtitle(subTitle) {
+    return Text(
+      subTitle,
+      style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w100),
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  thumbnail(imageUrl) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15.0),
+      child: CachedNetworkImage(
+        placeholder: (context, url) => Image.asset(placeholderImg),
+        imageUrl: imageUrl,
+        height: 50,
+        width: 70,
+        alignment: Alignment.center,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  rightIcon() {
+    return Icon(
+      Icons.keyboard_arrow_right,
+      color: Colors.grey,
+      size: 30.0,
+    );
+  }
+
+  isTabContain(List<String> keywords, String title) {
+    for (int j = 0; j < keywords.length; j++) {
+      if (title.toLowerCase().contains(keywords[j])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isAllTabContent(str) {
+    bool tmp = false;
+    if (isTabContain(higKeyword, str)) {
+      tmp = true;
+    }
+    if (isTabContain(junHigKeyword, str)) {
+      tmp = true;
+    }
+    if (isTabContain(eleKeyword, str)) {
+      tmp = true;
+    }
+    if (tmp == false && isTabContain(allTabKeyword, str)) {
+      return true;
+    } else
+      return false;
+  }
+
+  getItems() {
+    listItemDH.clear();
+    listItemCD.clear();
+    listItemC3.clear();
+    listItemC2.clear();
+    listItemC1.clear();
+    listItemAll.clear();
+    for (int x = 0; x < _feedList.length; x++) {
+      for (int i = 0; i < _feedList[x].items.length; i++) {
+        var item = ListItem();
+        item.title = _feedList[x].items[i].title;
+        item.subtitle = _feedList[x].items[i].pubDate;
+        item.imgUrl = getImgUrl(_feedList[x].items[i].description);
+        item.link = getUrl(_feedList[x].items[i].link);
+        listItemAll.add(item);
+        String str = _feedList[x].items[i].description;
+        if (isTabContain(uniKeyword, str)) {
+          listItemDH.add(item);
+        }
+        if (isTabContain(colKeyword, str)) {
+          listItemCD.add(item);
+        }
+        if (isTabContain(higKeyword, str) || isAllTabContent(str)) {
+          listItemC3.add(item);
+        }
+        if (isTabContain(junHigKeyword, str) || isAllTabContent(str)) {
+          listItemC2.add(item);
+        }
+        if (isTabContain(eleKeyword, str) || isAllTabContent(str)) {
+          listItemC1.add(item);
+        }
+      }
+    }
+  }
+
+  getImgUrl(str)
+  {
+    final imgTag = 'img';
+    final idx = str.indexOf(imgTag);
+    final subStr = str.substring(idx);
+    return getUrl(subStr);
+  }
+
+  getUrl( str) {
+    var urlPattern =
+        r"(https?|ftp)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
+    final imgSrc = new RegExp(urlPattern, caseSensitive: false)
+        .firstMatch(str)
+        .group(0);
+    return imgSrc;
+  }
+
+  list(listItem) {
+    //getTHCS();
+    return ListView.builder(
+      itemCount: listItem.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = listItem[index];
+
+        return ListTile(
+          title: title(item.title),
+          subtitle: subtitle(item.subtitle),
+          leading: thumbnail(item.imgUrl),
+          trailing: rightIcon(),
+          contentPadding: EdgeInsets.all(5.0),
+          onTap: () => openFeed(item.link),
+        );
+      },
+    );
+  }
+
+  isFeedEmpty() {
+    return null == _feedList;
+  }
+
+  body(listItem) {
+    return isFeedEmpty()
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : list(listItem);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double orjWidth = MediaQuery.of(context).size.width;
+    double cameraWidth = orjWidth / 24;
+    double yourWidth = (orjWidth - cameraWidth) / 4;
+    double tabHeight = 50.0;
+
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: ColorApp.backgroundColor,
+            centerTitle: true,
+            leading: Icon(Icons.arrow_back),
+            title: Text(
+              'Đọc báo',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            bottom: PreferredSize(
+                child: TabBar(
+                    isScrollable: true,
+                    unselectedLabelColor: Colors.white.withOpacity(0.3),
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text("TẤT CẢ"),
+                      ),
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text('ĐẠI HỌC'),
+                      ),
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text('CAO ĐẲNG'),
+                      ),
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text('CẤP 3'),
+                      ),
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text('CẤP 2'),
+                      ),
+                      Container(
+                        width: yourWidth,
+                        height: tabHeight,
+                        alignment: Alignment.center,
+                        child: Text('TIỂU HỌC'),
+                      ),
+                    ]),
+                preferredSize: Size.fromHeight(60.0)),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+              ),
+            ],
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(
+                child: body(listItemAll),
+              ),
+              Container(
+                child: Center(
+                  child: body(listItemDH),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: body(listItemCD),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: body(listItemC3),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: body(listItemC2),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: body(listItemC1),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
